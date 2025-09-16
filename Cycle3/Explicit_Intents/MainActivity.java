564c7ea3e9b4fe9c23105950713f8921ee389573
Anuradha-KP <chn24mc053@ceconline.edu>
2025-09-16 15:26:55 +0530

    Create MainActivity.java

---
+++ Cycle3/Explicit_Intents/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.explicit_intents17;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Button navigateButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //Set the layout for the main activity
+
+        //Find the button from the layout file by its ID.
+        navigateButton=findViewById(R.id.navigateButton);
+        //Set an OnClickListener on the button to handle clicks.
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //An explicit intent is used to launch a specific component(Activity).
+                //It requires two arguments:the current context and the target class.
+                Intent explicitIntent=new Intent(MainActivity.this,SecondActivity.class);
+                //Use startActivity() to execute the intent and launch the new activity.
+                startActivity(explicitIntent);
+            }
+        });
+    }
+}
