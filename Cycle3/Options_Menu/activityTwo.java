103412241d0227c6835648caa32b2cffac4b4f14
Anuradha-KP <chn24mc053@ceconline.edu>
2025-09-16 15:20:21 +0530

    Create activityTwo.java

---
+++ Cycle3/Options_Menu/activityTwo.java
@@ -0,0 +1,14 @@
+package com.example.optionsmenu17;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+//This is the second destination activity.
+public class activityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two); //Display the layout of this activity.
+    }
+}
